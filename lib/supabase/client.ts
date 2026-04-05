03103aca1b8f828c92ef6e86897518a71a0016dc
gt4065x <gt4065x@gmail.com>
2026-04-05 15:54:26 +0900

    fix: auth session +  supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,14 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
+let client: ReturnType<typeof createSupabaseClient> | null = null
+
 export function createClient() {
-  return createSupabaseClient(
+  if (client) return client
+
+  client = createSupabaseClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
+
+  return client
 }
